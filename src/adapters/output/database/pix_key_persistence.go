a6a0ba4daa45f571aec4646c83bb9027d0efb0d4
Danilo Y Kodavara <danilo.kodavara@hotmail.com>
2023-07-10 16:12:58 -0300

    feat: refactor pix_key_persistence export object.

--- src/adapters/output/database/pix_key_persistence.go
+++ src/adapters/output/database/pix_key_persistence.go
@@ -3,21 +3,22 @@ package database
 import (
 	"github.com/danyukod/cadastro-chave-pix-go/src/adapters/output/database/entity"
 	"github.com/danyukod/cadastro-chave-pix-go/src/adapters/output/database/mapper"
+	"github.com/danyukod/cadastro-chave-pix-go/src/application/ports/output"
 	"github.com/danyukod/cadastro-chave-pix-go/src/domain/pix_key"
 	"gorm.io/gorm"
 )
 
-type PixKeyPersistence struct {
-	db *gorm.DB
-}
-
-func NewPixKeyPersistence(db *gorm.DB) PixKeyPersistence {
-	return PixKeyPersistence{
+func NewPixKeyPersistence(db *gorm.DB) output.PixKeyPersistenceInterface {
+	return pixKeyPersistence{
 		db,
 	}
 }
 
-func (p PixKeyPersistence) CreatePixKey(pixKeyDomain pix_key.PixKeyDomainInterface) (pix_key.PixKeyDomainInterface, error) {
+type pixKeyPersistence struct {
+	db *gorm.DB
+}
+
+func (p pixKeyPersistence) CreatePixKey(pixKeyDomain pix_key.PixKeyDomainInterface) (pix_key.PixKeyDomainInterface, error) {
 	var pixKeyEntity entity.PixKeyEntity
 
 	pixKeyEntity = mapper.ConvertDomainToEntity(pixKeyDomain)
@@ -36,7 +37,7 @@ func (p PixKeyPersistence) CreatePixKey(pixKeyDomain pix_key.PixKeyDomainInterfa
 	return pixKeyDomain, nil
 }
 
-func (p PixKeyPersistence) FindPixKeyByKeyAndType(pixKeyType string, pixKey string) (pix_key.PixKeyDomainInterface, error) {
+func (p pixKeyPersistence) FindPixKeyByKeyAndType(pixKeyType string, pixKey string) (pix_key.PixKeyDomainInterface, error) {
 	var pixKeyEntity entity.PixKeyEntity
 
 	err := p.db.Where("pix_key_type = ?", pixKeyType).Where("pix_key = ?", pixKey).First(&pixKeyEntity).Error
