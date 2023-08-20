020d7bdbbe9308be55420833600e8752db6af908
Danilo Y Kodavara <danilo.kodavara@hotmail.com>
2023-08-20 18:26:58 -0300

    feat: refactor config.

--- configs/factories/pix_key_router_factory.go
+++ configs/factories/pix_key_router_factory.go
@@ -3,7 +3,7 @@ package factories
 import (
 	"github.com/danyukod/cadastro-chave-pix-go/docs"
 	"github.com/danyukod/cadastro-chave-pix-go/internal/presentation/rest/handler/factory"
-	routes2 "github.com/danyukod/cadastro-chave-pix-go/internal/presentation/rest/routes"
+	"github.com/danyukod/cadastro-chave-pix-go/internal/presentation/rest/routes"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -16,7 +16,7 @@ func NewPixKeyRouterFactory(database *gorm.DB) error {
 
 	router := gin.Default()
 	v1 := router.Group("/api/v1")
-	routes2.InitPixKeyRoutes(v1, pixKeyController)
+	routes.InitPixKeyRoutes(v1, pixKeyController)
 
 	docs.SwaggerInfo.BasePath = "/api/v1"
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
